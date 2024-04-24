a6b6edb39ce93c18ce32ab9b7389547da29160c1
AndrewG <151661194+AnReyX@users.noreply.github.com>
2024-04-24 21:47:08 +0200

    Add files via upload

--- Проект MathSageAi_bot.alpha.0.2/MathSageAi_bot_main.py
+++ Проект MathSageAi_bot.alpha.0.2/MathSageAi_bot_main.py
@@ -1,113 +1,136 @@
 import logging
 from telegram.ext import Application, MessageHandler, filters
-from config import BOT_TOKEN
+from telegram import ReplyKeyboardMarkup, InputMediaPhoto
+from BOT_TOKEN import BOT_TOKEN
 from Bot_DB import BOT_DATA_BASE
 from Bot_Simple_Prases import SIMPLE_PRASES
 from telegram.ext import CommandHandler
-import requests
-import aiohttp
 import time
 
-
-async def geocoder(update, context):
-    context2 = update.message.text.split()[1]
-    geocoder_uri = "http://geocode-maps.yandex.ru/1.x/"
-    response = await get_response(geocoder_uri, params={
-        "apikey": "40d1649f-0493-4b70-98ba-98533de7710b",
-        "format": "json",
-        "geocode": context2
-    })
-
-    toponym = response["response"]["GeoObjectCollection"][
-        "featureMember"][0]["GeoObject"]
-    ll, spn = get_ll_spn(toponym)
-    # Можно воспользоваться готовой функцией,
-    # которую предлагалось сделать на уроках, посвящённых HTTP-геокодеру.
-
-    static_api_request = f"http://static-maps.yandex.ru/1.x/?ll={ll}&spn={spn}&l=map"
-    await context.bot.send_photo(
-        update.message.chat_id,  # Идентификатор чата. Куда посылать картинку.
-        # Ссылка на static API, по сути, ссылка на картинку.
-        # Телеграму можно передать прямо её, не скачивая предварительно карту.
-        static_api_request,
-        caption="Нашёл:"
-    )
-
-
-def get_ll_spn(toponym):
-    # Координаты центра топонима:
-    toponym_coodrinates = toponym["Point"]["pos"]
-    # Долгота и Широта :
-    toponym_longitude, toponym_lattitude = toponym_coodrinates.split(" ")
-
-    # Собираем координаты в параметр ll
-    ll = ",".join([toponym_longitude, toponym_lattitude])
-
-    # Рамка вокруг объекта:
-    envelope = toponym["boundedBy"]["Envelope"]
-
-    # левая, нижняя, правая и верхняя границы из координат углов:
-    l, b = envelope["lowerCorner"].split(" ")
-    r, t = envelope["upperCorner"].split(" ")
-
-    # Вычисляем полуразмеры по вертикали и горизонтали
-    dx = abs(float(l) - float(r)) / 2.0
-    dy = abs(float(t) - float(b)) / 2.0
-
-    # Собираем размеры в параметр span
-    span = f"{dx},{dy}"
-
-    return ll, span
-
-
-async def get_response(url, params):
-    async with aiohttp.ClientSession() as session:
-        async with session.get(url, params=params) as resp:
-            return await resp.json()
-
-
-async def Message_Handler(update, context):
-    message = update.message.text.lower()
-    if 'посчитай' in message:
-        try:
-            await update.message.reply_text(eval(''.join(message.split()[1:])))
-        except:
-            await update.message.reply_text('Извините, но вы указали неверный пример')
-    elif 'время' in message:
-        await time_command(update, context)
-    elif 'дата' in message or 'день' in message:
-        await date_command(update, context)
-    elif 'покажи' in message:
-       # geocoder_request = "http://geocode-maps.yandex.ru/1.x/?apikey=40d1649f-0493-4b70-98ba-98533de7710b&geocode" \
-        #                   f"={message.split()[1:]}, 1&format=json"
-        await geocoder(update, context)
-        #await context.bot.sendPhoto(update.message.chat.id, geocoder_request, caption=update.message.text)
-            
-    elif 'расскажи о' in message or 'что такое' in message:
-        if len(message.split()) > 2:
-            if len(message.split()) == 3:
-                search_word = message.split()[2]
-                for i in range(0, len(search_word)):
-                    if search_word in BOT_DATA_BASE.keys():
-                        await update.message.reply_text(BOT_DATA_BASE[search_word])
-                        break
-                    search_word = search_word[:-1]
-                else:
-                    await update.message.reply_text('Извините, по вашему запросы ничего не нашлось')
-        else:
-            await update.message.reply_text('Извините, но вы не указали, что конкретно интересует вас, повторите запрос')
-    elif message in SIMPLE_PRASES.keys():
-        await update.message.reply_text(SIMPLE_PRASES[message])
-    else:
-        await update.message.reply_text('Извините, я вас не понял, повторите запрос')
+current_theme = ''
+definitions = {'logarithms': (open('DATA/logarithms/definition.txt', encoding='UTF-8'),
+                              open('DATA/logarithms/log.png', 'rb')),
+               'exponents': (open('DATA/exponents/definitions.txt', encoding='UTF-8'),
+                             open('DATA/exponents/def.jpg', 'rb')),
+               'stereometry': (open('DATA/stereometry/definition.txt', encoding='UTF-8'),
+                               open('DATA/stereometry/def.jpg', 'rb'))}
+graphics = {'logarithms': [InputMediaPhoto(open('DATA/logarithms/y=log(x)_1.png', 'rb'),
+                                           caption=open('DATA/logarithms/graph.txt', encoding='UTF-8').read()),
+                           InputMediaPhoto(open('DATA/logarithms/y=log(x)_2.png', 'rb'))],
+          'exponents': (open('DATA/logarithms/graph.txt'),
+                        open('DATA/exponents/y=√x.png', 'rb'), open('DATA/exponents/y=3^^√x.png', 'rb'),
+                        open('DATA/exponents/y=a^x_1.png', 'rb'), open('DATA/exponents/y=a^x_2.png', 'rb'),
+                        open('DATA/exponents/y=e^x.png', 'rb')),
+          'trigonometry': (open('DATA/trigonometry/graphs.txt'),
+                           open('DATA/trigonometry/y=sinx.png', 'rb'), open('DATA/trigonometry/y=cosx.png', 'rb'),
+                           open('DATA/trigonometry/y=tgx.png', 'rb'), open('DATA/trigonometry/y=ctgx.png', 'rb'),
+                           open('DATA/trigonometry/y=arcsinx.jpg', 'rb'), open('DATA/trigonometry/y=arccosx.jpg', 'rb'),
+                           open('DATA/trigonometry/y=arctgx.png', 'rb'), open('DATA/trigonometry/y=arcctgx.png', 'rb'))}
+rk_start = [['/commands', '/maths']]
+rk_commands = [['/comm_list', '/back_start']]
+rk_math = [['/algebra', '/stereometry', '/back_start']]
+rk_algebra = [['/logarithms', '/exponents', '/derivatives'], ['/probability', '/trigonometry', '/back_math']]
+rk_logarithms = [['/definition', '/properties', '/graphs'], ['/equations', '/tasks', '/back_algebra']]
+rk_exponents = [['/definition', '/properties', '/graphs'], ['/differentiating', '/tasks', '/back_algebra']]
+rk_derivatives = [['/derivs', '/integral', '/formulas'], ['/min-max', '/tasks', '/back_algebra']]
+rk_probability = [['/easy', '/complex', '/coin', '/dice'], ['/games', '/shooter', '/boxes', '/production'],
+                  ['/lines', '/tasks', '/back_algebra']]
+rk_trigonometry = [['/functions', '/degrees', '/circle', '/equations'],
+                   ['/graphs', '/formulas', '/tasks', '/back_algebra']]
+rk_stereometry = [['/definition', '/plane', '/easy_figs'], ['/sections', '/complex_figs', '/facts'],
+                  ['/trial_task', '/tasks', '/back_math']]
+mu_start = ReplyKeyboardMarkup(rk_start, one_time_keyboard=False)
+mu_commands = ReplyKeyboardMarkup(rk_commands, one_time_keyboard=False)
+mu_maths = ReplyKeyboardMarkup(rk_math, one_time_keyboard=False)
+mu_stereometry = ReplyKeyboardMarkup(rk_stereometry, one_time_keyboard=False)
+mu_algebra = ReplyKeyboardMarkup(rk_algebra, one_time_keyboard=False)
+mu_logarithms = ReplyKeyboardMarkup(rk_logarithms, one_time_keyboard=False)
+mu_exponents = ReplyKeyboardMarkup(rk_exponents, one_time_keyboard=False)
+mu_derivatives = ReplyKeyboardMarkup(rk_derivatives, one_time_keyboard=False)
+mu_probability = ReplyKeyboardMarkup(rk_probability, one_time_keyboard=False)
+mu_trigonometry = ReplyKeyboardMarkup(rk_trigonometry, one_time_keyboard=False)
 
 
 async def start(update, context):
-    await update.message.reply_text(f'Вас приветствует исскуственный интелект MathSageAi')
+    await update.message.reply_text('Вас приветствует искусственный интеллект MathSageAi. Что бы вы хотели сделать?',
+                                    reply_markup=mu_start)
 
 
-async def help_command(update, context):
-    await update.message.reply_text("Для работы бота нпиши расскажи о ... / что такое ... / посчитай ...")
+async def commands(update, context):
+    await update.message.reply_text("Для работы бота напиши расскажи о ... / что такое ... / посчитай ..."
+                                    " Нажми на /comm_list, чтобы получить информацию о командах!")
+
+
+async def definition(update, context):
+    if not current_theme:
+        await update.message.reply_text("Не могу понять, о чём вы хотите узнать. Сначала выберите тему.")
+    else:
+        await context.bot.send_media_group(
+            update.message.chat_id,
+            definitions[current_theme][1],
+            caption=definitions[current_theme][0].read()
+        )
+
+
+async def graphs(update, context):
+    if not current_theme:
+        await update.message.reply_text("Не могу понять, о каких графиках рассказать. Сначала выберите тему.")
+    else:
+        await context.bot.send_media_group(
+            chat_id=update.message.chat_id,
+            media=graphics[current_theme],
+        )
+
+
+async def math(update, context):
+    global current_theme
+    current_theme = ''
+    await update.message.reply_text("Выберите, о чём хотите узнать - об алгебре или стереометрии?",
+                                    reply_markup=mu_maths)
+
+
+async def algebra(update, context):
+    global current_theme
+    current_theme = ''
+    await update.message.reply_text("Выберите раздел!\n1.Логарифмы\n2.Показательные и степенные"
+                                    "\n3.Производная и интеграл\n4.Теория вероятности\n5.Тригонометрия",
+                                    reply_markup=mu_algebra)
+
+
+async def logarithms(update, context):
+    global current_theme
+    current_theme = 'logarithms'
+    await update.message.reply_text("Выберите тему:", reply_markup=mu_logarithms)
+
+
+async def exponents(update, context):
+    global current_theme
+    current_theme = 'exponents'
+    await update.message.reply_text("Выберите тему:", reply_markup=mu_exponents)
+
+
+async def derivatives(update, context):
+    global current_theme
+    current_theme = 'derivatives'
+    await update.message.reply_text("Выберите тему:", reply_markup=mu_derivatives)
+
+
+async def probability(update, context):
+    global current_theme
+    current_theme = 'probability'
+    await update.message.reply_text("Выберите тему:", reply_markup=mu_probability)
+
+
+async def trigonometry(update, context):
+    global current_theme
+    current_theme = 'trigonometry'
+    await update.message.reply_text("Выберите тему:", reply_markup=mu_trigonometry)
+
+
+async def stereometry(update, context):
+    global current_theme
+    current_theme = 'stereometry'
+    await update.message.reply_text("Выберите тему:", reply_markup=mu_stereometry)
 
 
 async def date_command(update, context):
@@ -118,15 +141,56 @@ async def time_command(update, context):
     await update.message.reply_text(f"Текущее время: {time.asctime().split()[3]}")
 
 
+async def Message_Handler(update, context):
+    message = update.message.text.lower()
+    if 'посчитай' in message:
+        try:
+            await update.message.reply_text(eval(''.join(message.split()[1:])))
+        except Exception:
+            await update.message.reply_text('Извините, но вы указали неверный пример')
+    elif 'время' in message:
+        await update.message.reply_text(f"Текущее время: {', '.join(time.asctime().split()[3])}")
+    elif 'дата' in message or 'день' in message:
+        await update.message.reply_text(f"Текущая дата: {time.asctime().split()[1:3]}")
+    elif 'расскажи о' in message or 'что такое' in message:
+        if len(message.split()) > 2:
+            if len(message.split()) == 3:
+                search_word = message.split()[2]
+                for i in range(0, len(search_word)):
+                    if search_word in BOT_DATA_BASE.keys():
+                        await update.message.reply_text(BOT_DATA_BASE[search_word])
+                        break
+                    search_word = search_word[:-1]
+                else:
+                    await update.message.reply_text('Извините, по вашему запросу ничего не нашлось.')
+        else:
+            await update.message.reply_text('Извините, но я не понял, что именно вы хотите узнать. Повторите запрос.')
+    else:
+        await update.message.reply_text('Извините, я вас не понял. Повторите запрос.')
+
+
 def main():
     application = Application.builder().token(BOT_TOKEN).build()
     text_handler = MessageHandler(filters.TEXT & ~filters.COMMAND, Message_Handler)
     application.add_handler(text_handler)
 
     application.add_handler(CommandHandler("start", start))
-    application.add_handler(CommandHandler("help", help_command))
+    application.add_handler(CommandHandler("maths", math))
+    application.add_handler(CommandHandler("commands", commands))
     application.add_handler(CommandHandler("date", date_command))
     application.add_handler(CommandHandler("time", time_command))
+    application.add_handler(CommandHandler("back_start", start))
+    application.add_handler(CommandHandler("back_math", math))
+    application.add_handler(CommandHandler("back_algebra", algebra))
+    application.add_handler(CommandHandler("algebra", algebra))
+    application.add_handler(CommandHandler("stereometry", stereometry))
+    application.add_handler(CommandHandler("logarithms", logarithms))
+    application.add_handler(CommandHandler("exponents", exponents))
+    application.add_handler(CommandHandler("derivatives", derivatives))
+    application.add_handler(CommandHandler("probability", probability))
+    application.add_handler(CommandHandler("trigonometry", trigonometry))
+    application.add_handler(CommandHandler("definition", definition))
+    application.add_handler(CommandHandler("graphs", graphs))
 
     application.run_polling()
 
